844c0637037bc0ae95de06e32d6ddf0d799fd286
Uchida <uchihachikk3@gmail.com>
2026-01-30 15:33:34 +0700

    homework

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class Character {
+
+    private int health;
+    private int stamina;
+    private final int maxHealth ;
+    private final int maxStamina ;
+
+    public Character(int health,int stamina){
+        this.health = health;
+        this.stamina = stamina;
+        this.maxStamina = stamina;
+        this.maxHealth= health;
+        System.out.println("Character created with "+health+" health and "+stamina+" stamina.");
+
+    }
+
+    public void Status(){
+        System.out.println("Character's health: "+health+"., stamina: "+stamina);
+    }
+    public void  swingSword(){
+        stamina -= 10;
+        if(stamina<=0){
+            System.out.println("Your character needs to cool down.");
+        }else {
+            System.out.println("Character swings the sword. Stamina is now " + stamina);
+        }
+    }
+    public void takeDamage(int damage){
+        if(health<damage){
+            System.out.println("Character is dead.");
+        }else {
+            health-=damage;
+            System.out.println("Character takes " + damage + " damage. Health is now " + health);
+        }
+    }
+    public void rest(){
+        stamina = maxStamina;
+        health = maxHealth;
+        System.out.println("Character has rested. Health and stamina restored to maximum.");
+    }
 }
+
